df7e6cf7fad718e86d32bcfbde12e5f8b0423d72
moonlight83340 <moonlight83340@users.noreply.github.com>
2021-11-25 22:51:57 +0100

    add configuration, to add multiple addons to install

---
+++ config.md
@@ -0,0 +1,5 @@
+**WARNING**: Do not change these options if you're planning to share your deck
+with others, as reading generation will not work for other users if you change
+the defaults.
+
+*addons*: list of addon ID's.
--- dependenciesInstall.py
+++ dependenciesInstall.py
@@ -1,5 +1,6 @@
 from aqt import mw
 from aqt.addons import download_addons
+config = mw.addonManager.getConfig(__name__)
 
 try:
     from aqt import gui_hooks
@@ -11,13 +12,18 @@ def on_done(info)-> None:
 
 # download only AnkiConnect, should be use with a configuration (to add more dependencies, multiple dependencies configurations to do)   
 
-def dependenciesAddons():    
-    if("2055492159" not in mw.addonManager.allAddons()):
-        download_addons(mw, mw.addonManager, [2055492159], on_done)
-        configAnkiConnect = mw.addonManager.getConfig("2055492159")
+def dependenciesAddonsInstall():    
+    for id in config['addons']:
+        if(id not in mw.addonManager.allAddons()):
+            download_addons(mw, mw.addonManager, [id], on_done)
+            
+def dependenciesAddonsConfig():
+# configuration for Anki Connect
+    configAnkiConnect = mw.addonManager.getConfig("2055492159")
+    if "https://mezase.herokuapp.com" not in configAnkiConnect['webCorsOriginList']
         configAnkiConnect['webCorsOriginList'] += ["https://mezase.herokuapp.com"]
-	mw.addonManager.writeConfig("2055492159",configAnkiConnect)
+        mw.addonManager.writeConfig("2055492159",configAnkiConnect)
 
 # Attach function to window did init hook (if GUI hooks are available)
 if gui_hooks:
-    gui_hooks.main_window_did_init.append(dependenciesAddons)
+    gui_hooks.main_window_did_init.append(dependenciesAddonsInstall)
